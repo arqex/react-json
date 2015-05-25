8c253705ee2eeede71a9528002f611e4e854d870
marquex <javi@arqex.com>
2015-05-25 20:13:54 +0200

    Set up the build system

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# json-form
+# react-json
 A JSON editor packed as a React.js component, but also the simplest way of creating web forms.
---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp'),
+	uglify = require('gulp-uglify'),
+	insert = require('gulp-insert'),
+	webpack = require('gulp-webpack')
+;
+
+var packageName = 'react-json';
+var pack = require( './package.json' );
+
+var getWPConfig = function( filename ){
+	return {
+		externals: {
+			react: 'react'
+		},
+		output: {
+			libraryTarget: 'umd',
+			filename: filename + '.js'
+		}
+	};
+};
+
+var cr = ('/*\n%%name%% v%%version%%\n%%homepage%%\n%%license%%: https://github.com/arqex/' + packageName + '/raw/master/LICENSE\n*/\n')
+	.replace( '%%name%%', pack.name)
+	.replace( '%%version%%', pack.version)
+	.replace( '%%license%%', pack.license)
+	.replace( '%%homepage%%', pack.homepage)
+;
+
+function wp( config, minify ){
+	var stream =  gulp.src('./Json.js')
+		.pipe( webpack( config ) )
+	;
+
+	if( minify ){
+		stream.pipe( uglify() );
+	}
+
+	return stream.pipe( insert.prepend( cr ) )
+		.pipe( gulp.dest('build/') )
+	;
+}
+
+gulp.task("build", function( callback ) {
+	var config = getWPConfig( 'Json' );
+	config.devtool = '#eval';
+	wp( config );
+
+	config = getWPConfig( 'Json.min' );
+	wp( config, true );
+
+	config = getWPConfig( 'Json-no-freezer' );
+	config.devtool = '#eval';
+	config.externals['freezer-js'] = 'freezer-js';
+	wp( config );
+
+	config = getWPConfig( 'Json-no-freezer.min' );
+	config.externals['freezer-js'] = 'freezer-js';
+	return wp( config, true );
+});
+
+gulp.task( 'default', ['build'] );
